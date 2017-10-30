40639834254cff1553cb2b94df24757380b4cbda
Julian <jinw@protonmail.com>
2017-10-30 23:17:57 +0000

    Complete with conversation

---
+++ src/components/Conversation.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-for="item in conversation">
+      <p>{{item.person}}</p>
+      <p>{{item.message}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  
+  export default {
+    name: 'Conversation',
+    props: ['conversation']
+  }
+</script>
+<style scope>
+</style>
--- src/components/QuoteItem.vue
+++ src/components/QuoteItem.vue
@@ -4,13 +4,18 @@
 
         {{quoteData.rating}}
 
+        <Conversation v-bind:conversation="quoteData.conversation">
+        </Conversation>
     </div>
 </template>
 
 <script>
+import Conversation from '../components/Conversation'
+
 export default {
   name: 'QuoteItem',
-  props: ['quoteData']
+  props: ['quoteData'],
+  components: {Conversation: Conversation}
 }
 </script>
 
