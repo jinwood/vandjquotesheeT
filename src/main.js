bbb805f0e5938295fefd03a775b19fe42d8e1d81
Julian <jinw@protonmail.com>
2017-10-31 21:29:24 +0000

    Fix for weird vue bugx

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import App from './App'
 
+let originalVueComponent = Vue.component
+Vue.component = function (name, definition) {
+  if (name === 'bFormCheckboxGroup' || name === 'bCheckboxGroup' ||
+      name === 'bCheckGroup' || name === 'bFormRadioGroup') {
+    definition.components = {bFormCheckbox: definition.components[0]}
+  }
+  originalVueComponent.apply(this, [name, definition])
+}
+Vue.use(BootstrapVue)
+Vue.component = originalVueComponent
+
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
